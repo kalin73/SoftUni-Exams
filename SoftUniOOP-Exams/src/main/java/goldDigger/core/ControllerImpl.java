21ae5c8418714bcf37464782782eed32ea22865f
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-06-29 01:42:09 +0300

    solution

--- SoftUniOOP-Exams/src/main/java/goldDigger/core/ControllerImpl.java
+++ SoftUniOOP-Exams/src/main/java/goldDigger/core/ControllerImpl.java
@@ -1,11 +1,16 @@
 package goldDigger.core;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import goldDigger.commands.AddDiscovererCommand;
 import goldDigger.commands.AddSpotCommand;
 import goldDigger.commands.ExcludeDiscovererCommand;
-import goldDigger.commands.GetStatisticsCommand;
-import goldDigger.commands.InspectSpotCommand;
+import goldDigger.common.ConstantMessages;
+import goldDigger.common.ExceptionMessages;
 import goldDigger.models.discoverer.Discoverer;
+import goldDigger.models.operation.Operation;
+import goldDigger.models.operation.OperationImpl;
 import goldDigger.models.spot.Spot;
 import goldDigger.repositories.DiscovererRepository;
 import goldDigger.repositories.Repository;
@@ -14,6 +19,7 @@ import goldDigger.repositories.SpotRepository;
 public class ControllerImpl implements Controller {
 	private Repository<Discoverer> discoverers;
 	private Repository<Spot> spots;
+	private int spotCount;
 
 	public ControllerImpl() {
 		this.discoverers = new DiscovererRepository();
@@ -38,11 +44,54 @@ public class ControllerImpl implements Controller {
 
 	@Override
 	public String inspectSpot(String spotName) {
-		return new InspectSpotCommand(discoverers, spots, spotName).execute();
+		List<Discoverer> discoverers = this.discoverers.getCollection()
+				.stream()
+				.filter(d -> d.getEnergy() > 45)
+				.collect(Collectors.toList());
+		
+		if (discoverers.isEmpty()) {
+			throw new IllegalArgumentException(ExceptionMessages.SPOT_DISCOVERERS_DOES_NOT_EXISTS);
+		}
+		
+		Spot spot = this.spots.byName(spotName);
+		Operation operation = new OperationImpl();
+		
+		operation.startOperation(spot, discoverers);
+		long excluded = discoverers.stream().filter(d -> d.getEnergy() == 0).count();
+		this.spotCount++;
+
+		return String.format(ConstantMessages.INSPECT_SPOT, spotName, excluded);
 	}
 
 	@Override
 	public String getStatistics() {
-		return new GetStatisticsCommand(discoverers).execute();
+		String exhibits = "";
+		StringBuilder result = new StringBuilder();
+		
+		result.append(String.format(ConstantMessages.FINAL_SPOT_INSPECT, this.spotCount));
+		result.append(System.lineSeparator());
+		result.append(ConstantMessages.FINAL_DISCOVERER_INFO);
+		result.append(System.lineSeparator());
+
+		for (Discoverer discoverer : discoverers.getCollection()) {
+			result.append(String.format(ConstantMessages.FINAL_DISCOVERER_NAME, discoverer.getName()));
+			result.append(System.lineSeparator());
+			result.append(String.format(ConstantMessages.FINAL_DISCOVERER_ENERGY, discoverer.getEnergy()));
+			result.append(System.lineSeparator());
+
+			if (discoverer.getMuseum().getExhibits().isEmpty()) {
+				exhibits = "None";
+
+			} else {
+				exhibits = discoverer.getMuseum()
+						.getExhibits()
+						.stream()
+						.collect(Collectors.joining(ConstantMessages.FINAL_DISCOVERER_MUSEUM_EXHIBITS_DELIMITER));
+			}
+			result.append(String.format(ConstantMessages.FINAL_DISCOVERER_MUSEUM_EXHIBITS, exhibits));
+			result.append(System.lineSeparator());
+		}
+
+		return result.toString().trim();
 	}
 }
